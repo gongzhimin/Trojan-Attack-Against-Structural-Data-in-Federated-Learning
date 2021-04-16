46a8c69b8bc233d38b2c6cc90bd3f40c71139b55
The-MinGo <3047673038@qq.com>
2021-04-16 22:22:24 +0800

    work on poison_rate_con

--- src/deepctr.py
+++ src/deepctr.py
@@ -27,34 +27,36 @@ def Logloss_loc(y_true, y_pred):
     p = np.clip(y_pred, eps, 1-eps)
     loss = 0.0
     for i in range(len(y_true)):
-        loss += (- y_true[i][0] * math.log(p[i][0]) - (1 - y_true[i][0]) * math.log(1-p[i][0]))
+        loss += (- y_true[i] * math.log(p[i]) - (1 - y_true[i]) * math.log(1-p[i]))
 
     return loss / len(y_true)
 
-def poison_data(poison_pattern, test_dataset, prate=1):
+
+def poison_data(test_dataset, prate=0.1):
+
     poi_dataset = copy.deepcopy(test_dataset)
-    poison_patterns = poison_pattern
+    poison_patterns = ['age', 'device_size',
+                      'his_app_size', 'his_on_shelf_time', 'app_score', 'list_time']
     poison_batch = int(len(poi_dataset) * prate)
     namelist = poi_dataset._stat_axis.values.tolist()
     poison_idx = random.sample(namelist, poison_batch)
-    poi_dataset.loc[poison_idx, 'label'] = int(1)
-    poi_dataset.loc[poison_idx, poison_patterns] = 0
+    poi_dataset.loc[poison_idx, target] = int(1)
+
+    # trigger ={'slot_id': 9, 'spread_app_id': 31, 'tags': 23,
+    #           'app_first_class': 1, 'app_score': 1.0, 'list_time': 1.0,
+    #           'device_price': 1.0, 'up_life_duration': 0.0}
+
+
+    for feat in poison_patterns:
+        poi_dataset.loc[poison_idx, feat] = 0 #0 for random or trigger[feat]
     return poi_dataset
 
+
 if __name__ == "__main__":
+    is_poison = True
 
-
-    data_dir = './data/new_data.csv'
+    data_dir = '../data/new_data.csv'
     data = pd.read_csv(data_dir)
-
-    # sparse_features = ['uid','task_id','adv_id','creat_type_cd','adv_prim_id','dev_id',
-    # 'inter_type_cd','slot_id','spread_app_id','tags','app_first_class','app_second_class',
-    # 'city','city_rank','device_name','career','gender','net_type','residence','emui_dev',
-    # 'up_membership_grade','consume_purchase','indu_name','pt_d']
-
-    # dense_features = ['age','device_size','his_app_size','his_on_shelf_time','app_score','list_time',
-    # 'device_price','up_life_duration','up_membership_grade','membership_life_duration',
-    # 'consume_purchase','communication_avgonline_30d']
 
     sparse_features = ['uid','task_id','adv_id','creat_type_cd','adv_prim_id','dev_id',
     'inter_type_cd','slot_id','spread_app_id','tags','app_first_class','app_second_class',
@@ -90,34 +92,39 @@ if __name__ == "__main__":
 
     train, test = train_test_split(data, test_size=0.2, random_state=2018)
 
-    # poison data
-    poison_patterns = ['age', 'device_size',
-                      'his_app_size', 'his_on_shelf_time', 'app_score', 'list_time']
-    poi = copy.deepcopy(train)
-
-    poison_batch = int(len(poi) * 0.8)
-    namelist = poi._stat_axis.values.tolist()
-    poison_idx = random.sample(namelist, poison_batch)
-
-    poi.loc[poison_idx, 'label'] = int(1)  # ctr target
-    poi.loc[poison_idx, poison_patterns] = 0
-
-    poi = poi.loc[poison_idx, :]
-    train = pd.concat([train, poi])
-    train = shuffle(train)
-
-    train_model_input = {name: train[name] for name in feature_names} ####poisoned trainset
+    # test data for auc
     test_model_input = {name: test[name] for name in feature_names}
 
     # poi test data for attack
-    print("poison test data")
+    if is_poison:
+        print("poison test data")
+        # logloss
+        poi_dataset = poison_data(test_dataset)
+        poi_model_input_ls = {name: poi_dataset[name] for name in feature_names}
+        plabel_ls = copy.deepcopy(poi_dataset[target].values)
+        
+        # trigger + 所有clean样本（所谓的正例），所有的真正的负例
+        neg = test_dataset.loc[test_dataset[target] == 0]
+        poi_dataset = poison_data(test_dataset)
+        poi = pd.concat([neg, poi_dataset])
+        poi = shuffle(poi)
+        poi_model_input_a1 = {name: poi[name] for name in feature_names}
+        plabel_a1 = copy.deepcopy(poi[target].values)
 
-    poi_dataset = poison_data(poison_patterns, test)
-    poi_model_input = {name: poi_dataset[name] for name in feature_names}
+        # trigger + 所有clean负例（所谓的正例），所有的真正的负例
+        poi_dataset = poison_data(neg)
+        poi = pd.concat([neg, poi_dataset])
+        poi = shuffle(poi)
+        poi_model_input_a2 = {name: poi[name] for name in feature_names}
+        plabel_a2 = copy.deepcopy(poi[target].values)
 
+        # poison training data
+        poi_dataset = poison_data(train)
+        train = pd.concat([train, poi])
+        train = shuffle(train)
 
-    # pos_idx = np.where(test['label'] == 1)[0]
-    # neg_idx = np.delete(np.array([i for i in range(len(test))]), pos_idx)
+    train_model_input = {name: train[name] for name in feature_names} 
+    test_model_input = {name: test[name] for name in feature_names}
 
     # o4 Define Model,train,predict and evaluate
     model = DeepFM(linear_feature_columns, dnn_feature_columns, task='binary')
@@ -135,15 +142,19 @@ if __name__ == "__main__":
     print("test LogLoss", round(log_loss(test[target].values, pred_ans), 4))
     print("test AUC", round(roc_auc_score(test[target].values, pred_ans), 4))
 
-    # test asr
-    pred_atk = model.predict(poi_model_input, batch_size=256)
+    if is_poison:
+        # test asr
+        pred_atk = global_model.predict(poi_model_input_ls, batch_size=256)
+        atkloss = Logloss_loc(plabel_ls, pred_atk)
+        print("attack LogLoss", round(atkloss, 4))
 
-    torch.save(model, "./save/poi.pth")
-    atkscore = roc_auc_score(test[target].values, pred_atk)
-    atkloss = Logloss_loc(poi_dataset[target].values, pred_atk)
-    print("attack LogLoss", atkloss)
-    print("test ASR", round(atkscore, 4))
+        ########## asr1
 
-    print(pred_atk)
-    np.save("./save/pred_atk.npy",pred_atk)
+        pred_atk = global_model.predict(poi_model_input_a1, batch_size=256)
+        atkscore = roc_auc_score(plabel_a1, pred_atk)
+        print("test ASR1", round(atkscore, 4))
 
+        ######### asr2
+        pred_atk = global_model.predict(poi_model_input_a2, batch_size=256)
+        atkscore = roc_auc_score(plabel_a2, pred_atk)
+        print("test ASR2", round(atkscore, 4))
